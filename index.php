83bd4424fb11949a259ac1c80e788877ab7e9006
Jonez de Mello Toffani da Silva <jmts39@hotmail.com>
2022-07-29 12:22:15 -0300

    finished
    Terminei.

--- index.php
+++ index.php
@@ -13,5 +13,20 @@ include 'include/class-autoload.inc.php';
 </head>
 <body>
     
+    <form action="include/calc.inc.php" method="post">
+        <p>My own Calculator</p>
+        <input type="number" name="num1" placeholder="First number">
+        <select name="oper">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select>
+        <input type="number" name="num2" placeholder="Second number">
+        <button type="submit" name="submit">Calculator </button>
+
+    </form>
+
+
 </body>
 </html>
